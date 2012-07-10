949acb5aea5db8593f429e23cc01d64dcd56e20b
Mike Podonyi <mikepodonyi@gmail.com>
2012-07-10 17:29:55 +0100

    first provider and Mef

--- src/SocialStream.Lib/StreamFactory.cs
+++ src/SocialStream.Lib/StreamFactory.cs
@@ -1,9 +1,36 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition;
 
 namespace SocialStream.Lib
 {
     public static class StreamFactory
     {
+        private static CompositionContainer container;
+
+        private  static void Compose()
+        {
+        //An aggregate catalog that combines multiple catalogs
+            var catalog = new DirectoryCatalog("");
+        //Adds all the parts found in the same assembly as the Program class
+        
+
+        //Create the CompositionContainer with the parts in the catalog
+            container = new CompositionContainer(catalog);
+
+        //Fill the imports of this object
+        try
+        {
+            container.ComposeParts();
+        }
+        catch (CompositionException compositionException)
+        {
+            Console.WriteLine(compositionException.ToString());
+        }
+        }
+
+
+
         private IEnumerable<StreamItem> GetStreamItems()
         {
 
